7b39bd3c3306885920d48989da64102c4c3efa96
KiesheaH <59402191+KiesheaH@users.noreply.github.com>
2025-08-24 13:50:02 -0400

    Join variables to form a string
    Used template literals to construct a sentence from multiple variables.

--- 05_joinStrings/joinStrings.spec.js
+++ 05_joinStrings/joinStrings.spec.js
@@ -1,28 +1,30 @@
-const values = require('./joinStrings')
+const values = require("./joinStrings");
 
-describe('step 2', () => {
-  test('firstName is Carlos', () => {
-    expect(values.firstName).toEqual('Carlos');
+describe("step 2", () => {
+  test("firstName is Kieshea", () => {
+    expect(values.firstName).toEqual("Kieshea");
   });
-  test.skip('lastName is Stevenson', () => {
-    expect(values.lastName).toEqual('Stevenson');
+  test("lastName is Hepburn", () => {
+    expect(values.lastName).toEqual("Hepburn");
   });
-  test.skip('thisYear is 1965', () => {
-    expect(values.thisYear).toEqual(1965);
+  test("thisYear is 2025", () => {
+    expect(values.thisYear).toEqual(2025);
   });
-  test.skip('birthYear is 1947', () => {
-    expect(values.birthYear).toEqual(1947);
+  test("birthYear is 1987", () => {
+    expect(values.birthYear).toEqual(1987);
   });
-  test.skip('greeting is properly output', () => {
-    expect(values.greeting).toEqual('Hello! My name is Carlos Stevenson and I am 18 years old.');
+  test("greeting is properly output", () => {
+    expect(values.greeting).toEqual(
+      "Hello! My name is Kieshea Hepburn and I am 38 years old."
+    );
   });
 });
 
-describe('step 3', () => {
-  test.skip('fullName is Carlos Stevenson', () => {
-    expect(values.fullName).toEqual('Carlos Stevenson');
+describe("step 3", () => {
+  test("fullName is Kieshea Hepburn", () => {
+    expect(values.fullName).toEqual("Kieshea Hepburn");
   });
-  test.skip('age is 18', () => {
-    expect(values.age).toEqual(18);
+  test("age is 38", () => {
+    expect(values.age).toEqual(38);
   });
 });
